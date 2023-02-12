59d7c5007dc6daf8db4fd23e92c9eef10d488128
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-12 16:36:27 +0100

    defined hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type PocketBase from 'pocketbase';
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: PocketBase,
+			user: any
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
