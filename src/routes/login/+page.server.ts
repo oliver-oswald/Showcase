25daaafb2a9264c757c014be1655837ad5df6740
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-13 16:11:12 +0100

    added login page

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,22 @@
+import { error, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+    login: async ({ locals, request }) => { 
+        const body = Object.fromEntries(await request.formData());
+
+        try {
+            await locals.pb.collection('users').authWithPassword(body.email as string, body.password as string);
+            if (!locals.pb?.authStore?.model?.verified) {
+                locals.pb.authStore.clear();
+                return {
+                    notVerified: true
+                }
+            }
+        } catch (err) {
+            console.error(err);
+            throw error(500, "something went wrong logging in")
+        }
+        throw redirect(303, '/');
+    }
+}
