ce37986b49e4ddf5eada1099afc616882ae89cbc
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-15 14:18:53 +0100

    added update password page

---
+++ src/routes/my/settings/security/+page.server.ts
@@ -0,0 +1,18 @@
+import { error, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+	updatePassword: async ({ request, locals }) => { 
+		const data = Object.fromEntries(await request.formData());
+
+		try {
+			await locals.pb.collection('users').update(locals.user?.id as string, data);
+			locals.pb.authStore.clear();
+		} catch (err) {
+			console.error(err);
+			throw error(400, 'Error updating password');
+		}
+		throw redirect(303, '/login');
+		
+	}
+};
