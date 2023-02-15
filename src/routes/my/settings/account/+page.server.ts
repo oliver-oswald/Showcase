22d327d846506163e2e7429d192180b9dfbd2397
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-15 13:32:31 +0100

    added change username

--- src/routes/my/settings/account/+page.server.ts
+++ src/routes/my/settings/account/+page.server.ts
@@ -15,5 +15,29 @@ export const actions: Actions = {
         return {
             success: true
         }
+    },
+    updateUsername: async ({ request, locals }) => { 
+        const data = Object.fromEntries(await request.formData());
+
+        try {
+            await locals.pb.collection('users').getFirstListItem(`username = "${data.username}"`)
+        } catch (err: any) {
+            if (err.status === 404) {
+                try {
+                    const { username } = await locals.pb.collection('users').update(locals.user?.id as string, { username: data.username });
+                    if (locals.user) {
+                        locals.user.username = username;
+                    }
+                    return {
+                        success: true
+                    }
+                } catch (err) {
+                    console.error(err);
+                    throw error(400, 'Error updating username');
+                }
+            }
+            console.error(err);
+            throw error(err.status, err.message);
+        }
     }
 };
--- src/routes/my/settings/account/+page.svelte
+++ src/routes/my/settings/account/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import { Input, Modal } from "$lib/components";
+	import type { PageData } from "./$types";
 
     export let form: any;
+    export let data: PageData;
 
     let emailModalOpen: boolean;
+    let usernameModalOpen: boolean;
 
     $: emailModalOpen = false;
+    $: usernameModalOpen = false;
 </script>
 
 <div class="flex flex-col w-full h-full space-y-12">
@@ -24,6 +28,25 @@
                     label="Enter your new email address"
                 />
                 <button type="submit" class="btn btn-primary w-full">Change my email</button>
+            </form>
+        </Modal>
+    </div>
+    <div class="w-full">
+        <h3 class="text-2xl font-medium">Change Username</h3>
+        <div class="divider mb-0.5"></div>
+        <Input id="username" label="Username" value={data.user?.username} disabled/>
+        <Modal label="change-username" checked={usernameModalOpen}>
+            <span slot="trigger" class="btn btn-primary">Change Username</span>
+            <h3 slot="heading">Change Your Username</h3>
+            <form action="?/updateUsername" method="POST" class="space-y-2">
+                <Input
+                    id="username"
+                    type="text"
+                    required
+                    value={form?.data?.username}
+                    label="Enter your new username address"
+                />
+                <button type="submit" class="btn btn-primary w-full">Change my username</button>
             </form>
         </Modal>
     </div>
