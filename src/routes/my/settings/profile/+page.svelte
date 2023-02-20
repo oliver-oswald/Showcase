fcd42670b3f240871df0e64d098a721d691c21c7
Oliver Oswald <oliver.oswald25@gmail.com>
2023-02-20 17:40:48 +0100

    fixed avatar on mobile

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -48,7 +48,7 @@
 		use:enhance={submitUpdateProfile}
 	>
 		<h3 class="text-2xl font-medium">Update Profile</h3>
-		<div class="form-control w-full max-w-lg">
+		<div class="form-control w-full aspect-square max-w-lg">
 			<label for="avatar" class="label font-medium pb-1">
 				<span class="label-text"> Profile Picture </span>
 			</label>
